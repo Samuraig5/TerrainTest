7a5a701b51e61f34fbe23ba4cf389ce41cc8235b
Joan Moser <104901510+Samuraig5@users.noreply.github.com>
2025-01-01 15:41:48 +0100

    Cleaned up the Camera class a bit

--- src/Rendering/Camera.java
+++ src/Rendering/Camera.java
@@ -8,12 +8,9 @@ public class Camera implements Translatable
 {
     JFrame window;
     Matrix4x4 projectionMatrix;
-    double aspectRatio;
     double fov = 90;
-    double fovRad;
     double zNear = 0.1d;
     double zFar = 1000;
-    double zNormal = zFar/(zFar-zNear);
 
     Vector3D position = new Vector3D();
     Vector3D rotation = new Vector3D(); // Rendering.Camera orientation (rotation in radians)
@@ -21,16 +18,7 @@ public class Camera implements Translatable
     public Camera(JFrame window)
     {
         this.window = window;
-        this.aspectRatio = (double) window.getHeight() / (double) window.getWidth();
-        this.fovRad = 1.0f / Math.tan(fov * 0.5f / 180.0f * Math.PI);
-
-        projectionMatrix = new Matrix4x4();
-        projectionMatrix.mat[0][0] = aspectRatio * fovRad;
-        projectionMatrix.mat[1][1] = fovRad;
-        projectionMatrix.mat[2][2] = zFar / (zFar - zNear);
-        projectionMatrix.mat[3][2] = (-zFar * zNear) / (zFar - zNear);
-        projectionMatrix.mat[2][3] = 1.0f;
-        projectionMatrix.mat[3][3] = 0.0f;
+        this.projectionMatrix = getProjectionMatrix();
     }
 
     /**
@@ -46,9 +34,22 @@ public class Camera implements Translatable
         return projectionMatrix.multiplyWithTriangle(in);
     }
 
-    public Vector3D getScreenDimensions()
-    {
+    public Vector3D getScreenDimensions() {
         return new Vector3D(window.getWidth(), window.getHeight(), 0);
+    }
+
+    private Matrix4x4 getProjectionMatrix() {
+        double aspectRatio = getScreenDimensions().y() / getScreenDimensions().x();
+        double fovRad = 1.0f / Math.tan(fov * 0.5f / 180.0f * Math.PI);
+
+        Matrix4x4 projectionMatrix = new Matrix4x4();
+        projectionMatrix.mat[0][0] = aspectRatio * fovRad;
+        projectionMatrix.mat[1][1] = fovRad;
+        projectionMatrix.mat[2][2] = zFar / (zFar - zNear);
+        projectionMatrix.mat[3][2] = (-zFar * zNear) / (zFar - zNear);
+        projectionMatrix.mat[2][3] = 1.0f;
+        projectionMatrix.mat[3][3] = 0.0f;
+        return projectionMatrix;
     }
 
     @Override
