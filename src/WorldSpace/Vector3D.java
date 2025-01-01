a0db67dc396fdc87f425be42aed98171e82f53fd
Joan Moser <104901510+Samuraig5@users.noreply.github.com>
2025-01-01 15:35:08 +0100

    Cleaned up the Vector3D class

--- src/WorldSpace/Vector3D.java
+++ src/WorldSpace/Vector3D.java
@@ -7,18 +7,25 @@ public class Vector3D implements Translatable
     private double z;
 
     public Vector3D(double x, double y, double z) {
-        this.x = x; this.y = y; this.z = z;
+        set(x, y, z);
     }
-    public Vector3D(){this.x=0; this.y=0; this.z=0;}
+    public Vector3D(){set(0,0,0);}
 
     public Vector3D(Vector3D source) {
-        this.x = source.x; this.y = source.y(); this.z = source.z();
+        set(source.x, source.y, source.z);
     }
     public double x() {return x;}
     public double y() {return y;}
     public double z() {return z;}
+    public void set(double newX, double newY, double newZ) {
+        this.x = newX; this.y = newY; this.z = newZ;
+    }
+    public void set(Vector3D source)
+    {
+        set(source.x, source.y, source.z);
+    }
     public void translate(double deltaX, double deltaY, double deltaZ){
-        this.x += deltaX; this.y += deltaY; this.z += deltaZ;
+        set(x+deltaX, y+deltaY, z+deltaZ);
     }
     @Override
     public void translate(Vector3D delta){
@@ -27,35 +34,24 @@ public class Vector3D implements Translatable
     public void translate(double uniformTranslation) {
         translate(uniformTranslation, uniformTranslation, uniformTranslation);
     }
+    /**
+     * Returns a new vector that is the result of translating this vector by a given vector.
+     * @param delta translation vector
+     * @return the result of the translation
+     */
     public Vector3D translation(Vector3D delta){
         Vector3D out = new Vector3D(this);
         out.translate(delta);
         return out;
     }
-
-    public void scale(Vector3D scalars)
-    {
-        set(x* scalars.x,y* scalars.y,z* scalars.z);
+    public void scale(Vector3D scalars) {
+        set(x*scalars.x,y*scalars.y,z*scalars.z);
     }
     public void scale(double scalar)
     {
         scale(new Vector3D(scalar, scalar, scalar));
     }
 
-    public void set(double newX, double newY, double newZ) {
-        this.x = newX; this.y = newY; this.z = newZ;
-    }
-    public void set(Vector3D source)
-    {
-        set(source.x, source.y, source.z);
-    }
-
-    /**
-     * @return The inverse of the vector.
-     */
-    public Vector3D inverse() {
-        Vector3D clone = new Vector3D(-x,-y,-z); return clone;
-    }
     /**
      * Inverts the vector.
      */
@@ -63,11 +59,19 @@ public class Vector3D implements Translatable
         set(-x,-y,-z);
     }
     /**
+     * @return The inverse of the vector.
+     */
+    public Vector3D inverse() {
+        Vector3D clone = new Vector3D(this);
+        clone.invert();
+        return clone;
+    }
+    /**
      * Normalizes the vector to length 1.
      */
     public void normalize(){
         double length = Math.sqrt(x*x+y*y+z*z);
-        x /= length; y /= length; z /= length;
+        set(x/length, y/length, z/length);
     }
     /**
      * Calculates the dot product of the two vectors.
