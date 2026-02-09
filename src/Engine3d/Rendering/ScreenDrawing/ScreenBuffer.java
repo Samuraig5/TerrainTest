52a359cbe31c2a247eb9d987ce7d43d5eb2201cf
Joan Moser <gian@qualit.ch>
2026-02-09 11:41:50 +0100

    Small overhead reduction by manipulating the pixel array of the ScreenBuffer>BufferedImage directly

--- src/Engine3d/Rendering/ScreenDrawing/ScreenBuffer.java
+++ src/Engine3d/Rendering/ScreenDrawing/ScreenBuffer.java
@@ -4,6 +4,7 @@ import Math.Vector.Vector3D;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import java.awt.image.WritableRaster;
 import java.util.Arrays;
 
@@ -12,6 +13,9 @@ import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 public class ScreenBuffer
 {
     private BufferedImage bufferedImage;
+    private int[] pixels;  // Direct pixel array
+    private int width;
+    private int height;
     private double[][] depthBuffer;
     private Object[] columnLocks;
 
@@ -21,9 +25,8 @@ public class ScreenBuffer
     }
 
     public void clear(int[] clearColour) {
-        WritableRaster raster = bufferedImage.getRaster();
-        int width = bufferedImage.getWidth(); int height = bufferedImage.getHeight();
-        raster.setPixels(0, 0, width, height, getSample(width, height, clearColour));
+        int clearARGB = (clearColour[3] << 24) | (clearColour[0] << 16) | (clearColour[1] << 8) | clearColour[2];
+        Arrays.fill(pixels, clearARGB);
         for (double[] column : depthBuffer) {
             Arrays.fill(column, 0.0);
         }
@@ -36,76 +39,46 @@ public class ScreenBuffer
         clear(colorToIntArray(c));
     }
     public void recompute(Vector3D screenSize) {
-        int width = (int) screenSize.x();
-        int height = (int) screenSize.y();
-        bufferedImage = new BufferedImage(width, height,TYPE_INT_ARGB);
+        width = (int) screenSize.x();
+        height = (int) screenSize.y();
+        bufferedImage = new BufferedImage(width, height, TYPE_INT_ARGB);
+        pixels = ((DataBufferInt) bufferedImage.getRaster().getDataBuffer()).getData(); //'pixels' points to memory space of BufferedImage pixel array
         depthBuffer = new double[width][height];
         columnLocks = new Object[width];
         for (int i = 0; i < width; i++) {
             columnLocks[i] = new Object();
         }
-
     }
 
     public boolean inBounds(int x, int y){
-        return (x >= 0 && x < bufferedImage.getWidth()) && (y >= 0 && y < bufferedImage.getHeight());
+        return (x >= 0 && x < width) && (y >= 0 && y < height);
     }
 
     public void writePixelIfOnTop(int x, int y, double depth, int colour) {
-        if (!inBounds(x, y)) return;
+        if (x < 0 || x >= width || y < 0 || y >= height) return;
 
-        try {
-            synchronized (columnLocks[x]) {
-                if (depth > depthBuffer[x][y]) {
-                    depthBuffer[x][y] = depth;
-                    int flippedY = getBufferedImage().getHeight() - y - 1;
-                    bufferedImage.setRGB(x, flippedY, colour);
-                }
+        synchronized (columnLocks[x]) {
+            if (depth > depthBuffer[x][y]) {
+                depthBuffer[x][y] = depth;
+                int flippedY = height - y - 1;
+                pixels[flippedY * width + x] = colour;  // Direct array write
             }
-        }
-        catch (IndexOutOfBoundsException e) {
-
         }
     }
 
     public void setPixel(int x, int y, int colour) {
-        if (!inBounds(x, y)) return;
+        if (x < 0 || x >= width || y < 0 || y >= height) return;
 
-        try {
-            //synchronized (columnLocks[x]) {
-            int flippedY = getBufferedImage().getHeight() - y - 1;
-            bufferedImage.setRGB(x, flippedY, colour);
-            //}
-        }
-        catch (IndexOutOfBoundsException e) {
-
-        }
+        int flippedY = height - y - 1;
+        pixels[flippedY * width + x] = colour;  // Direct array write
     }
 
     public BufferedImage getBufferedImage() {
         return bufferedImage;
     }
 
-    public int[] getSample(int width, int height, int[] colour)
-    {
-        int[] sample = new int[width * height * 4]; // 4 components per pixel (RGBA)
-
-        int red = colour[0];
-        int green = colour[1];
-        int blue = colour[2];
-        int alpha = colour[3];
-        for (int i = 0; i < width * height; i++) {
-            int baseIndex = i * 4; // Calculate the base index for each pixel (RGBA)
-            sample[baseIndex] = red;     // Set red
-            sample[baseIndex + 1] = green;   // Set green
-            sample[baseIndex + 2] = blue;    // Set blue
-            sample[baseIndex + 3] = alpha;   // Set alpha
-        }
-        return sample;
-    }
-
     public static int colorToARGB(Color color){
-        int alpha = color.getAlpha(); // Half transparent
+        int alpha = color.getAlpha();
         int red = color.getRed();
         int green = color.getGreen();
         int blue = color.getBlue();
